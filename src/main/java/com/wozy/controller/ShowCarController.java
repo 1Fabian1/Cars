5db81082a2e5e230d1227291205a172761c5d3ce
1Fabian1 <34246069+1Fabian1@users.noreply.github.com>
2019-08-06 20:01:34 +0200

    First Initial commit

--- src/main/java/com/wozy/controller/ShowCarController.java
+++ src/main/java/com/wozy/controller/ShowCarController.java
@@ -1,7 +1,32 @@
 package com.wozy.controller;
 
+import com.google.gson.Gson;
+import com.wozy.model.Car;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ShowCarController {
+
+    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
+    public String testCars(Model model){
+        String something = "AAAAAAA";
+        model.addAttribute("something", something);
+        return "test";
+    }
+
+
+
+    String address = "34.90.90.63/elasticsearch/auta/_search";
+    String body = "{\n" +
+            "  \"size\": 10,\n" +
+            "  \"from\": 0, \n" +
+            "  \"query\": {\n" +
+            "    \"match\": {\n" +
+            "      \"brand\": \"BMW\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
 }
--- src/main/webapp/WEB-INF/jsp/test.jsp
+++ src/main/webapp/WEB-INF/jsp/test.jsp
@@ -1,21 +1,17 @@
 <%--
   Created by IntelliJ IDEA.
   User: Fabia
-  Date: 23.07.2019
-  Time: 19:50
+  Date: 06.08.2019
+  Time: 20:46
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Welcome</title>
+    <title>Show Cars</title>
 </head>
 <body>
-<h1>Welcome :D</h1>
-<h2>${message}</h2>
-
-<a href="${pageContext.request.contextPath}/carList">carList</a>
-<a href="${pageContext.request.contextPath}/test">test</a>
+<h1>Hello on test</h1>
 
 
 </body>
